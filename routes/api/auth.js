cc129b6ab3d2c85586108a04ee33bde3d80b64ba
omprakashch3 <omprakashchautala3@gmail.com>
2020-08-26 17:24:08 +0530

    auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../model/User");
 
 //@route        GET api/Auth
 //@description  test routes
 //@access       public
-router.get("/", (req, res) => {
-  res.send("Auth routes...");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("serevr error");
+  }
 });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -64,8 +64,8 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        { expiresIn: 36000 },
-        (ERR, token) => {
+        { expiresIn: 360000 },
+        (err, token) => {
           if (err) throw err;
           res.json({ token });
         }
