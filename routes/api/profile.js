941b5cdcf0310e91bd3f52e78c35915eb5760db7
omprakashch3 <omprakashchautala3@gmail.com>
2020-09-01 20:38:35 +0530

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Profile = require("../../model/Profile");
+const User = require("../../model/User");
+const auth = require("../../middleware/auth");
+//@route        GET api/profile/me
+//@description  Get current user profile
+//@access       private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findone({
+      user: req.User.id,
+    }).populate("user", ["name", "avatar"]);
 
-//@route        GET api/profile
-//@description  test routes
-//@access       public
-router.get("/", (req, res) => {
-  res.send("Profile routes...");
+    if (!profile) {
+      return res.status(400).json({ msg: "there is no profile for this user" });
+    }
+    res.send(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
 
 module.exports = router;
